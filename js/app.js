14d89fded24353f522a4c3d1ea7f94b1c542f6e8
AnaSalazar <anastephany.salazarguzman@gmail.com>
2017-11-01 14:55:38 -0600

    agrega estilos y modal de sedes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 /*
  * Funcionalidad de tu proyecto
  */
+var modal = document.getElementById('modal');
+
+var dropdown = document.getElementById('dropDown');
+    dropdown.addEventListener("click", modalOpen);
+
+var close = document.getElementById("close");
+    close.addEventListener("click", modalClose);
+
+function modalOpen(){
+   modal.style.display = "block";
+ }
+
+function modalClose() {
+   modal.style.display = "none";
+}
