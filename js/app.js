4063c84823a22b5fd35b0b122fd0f116c0d39be5
AnaSalazar <anastephany.salazarguzman@gmail.com>
2017-11-03 11:38:21 -0600

    Agrega maquetación de overviews.html

--- js/app.js
+++ js/app.js
@@ -2,15 +2,22 @@
  * Funcionalidad de tu proyecto
  */
 var modal = document.getElementById('modal');
+var modalTech = document.getElementById('modalTech');
 
 var dropdown = document.getElementById('dropDown');
     dropdown.addEventListener("click", modalOpen);
 
+var dropDownTech = document.getElementById('dropDownTech');
+    dropDownTech.addEventListener("click", modalOpen);
+
 var close = document.getElementById("close");
     close.addEventListener("click", modalClose);
 
+var closeTech = document.getElementById("closeTech");
+    closeTech.addEventListener("click", modalClose);
+
 function modalOpen(){
-   modal.style.display = "block";
+   modalTech.style.display = "block";
  }
 
 function modalClose() {
